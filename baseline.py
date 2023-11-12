b53c3a856d3741c62729886416acdca8ef8ef042
Daisy <0109024@student.isf.edu.hk>
2023-11-11 17:56:46 -0800

    Added all of the setup/import stuff

---
+++ baseline.py
@@ -0,0 +1,26 @@
+"""
+Setup: setup all of the imports we need to do for this project
+"""
+
+import torch
+from torch import nn
+# from torchvision import transforms as T
+from PIL import Image
+import numpy as np
+from pathlib import Path
+from collections import deque
+import random, datetime, os
+
+# Gym is an OpenAI toolkit for RL
+import gym
+from gym.spaces import Box
+from gym.wrappers import FrameStack
+
+# NES Emulator for OpenAI Gym
+from nes_py.wrappers import JoypadSpace
+
+# Super Mario environment for OpenAI Gym
+import gym_super_mario_bros
+
+from tensordict import TensorDict
+from torchrl.data import TensorDictReplayBuffer, LazyMemmapStorage
